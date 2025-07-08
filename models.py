8a3ee9417bb0e61c3434a973dce8e00eeb477dd0
vaibhavanuragi2004 <vaibhavanuragi02@gmail.com>
2025-07-08 15:21:02 +0530

    Created  models.py

---
+++ models.py
@@ -0,0 +1,29 @@
+from app import db
+from datetime import datetime
+import json
+
+class TravelItinerary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(200), nullable=False)
+    duration = db.Column(db.Integer, nullable=False)  # days
+    budget = db.Column(db.Float, nullable=False)
+    interests = db.Column(db.Text)  
+    itinerary_data = db.Column(db.Text)  # JSON string of generated itinerary
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    checkpoints = db.relationship('Checkpoint', backref='itinerary', lazy=True, cascade='all, delete-orphan')
+    
+    def get_interests_list(self):
+        if self.interests:
+            return json.loads(self.interests)
+        return []
+    
+    def set_interests_list(self, interests_list):
+        self.interests = json.dumps(interests_list)
+    
+    def get_itinerary_data(self):
+        if self.itinerary_data:
+            return json.loads(self.itinerary_data)
+        return {}
+    
+    def set_itinerary_data(self, data):
+        self.itinerary_data = json.dumps(data)
